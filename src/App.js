fc570c71a5ae95859f4a6c1087bb03ad7f83677d
datnguyenner <dat.nguyen@seven.me>
2021-01-22 09:22:53 -0800

    Add test data

--- src/App.js
+++ src/App.js
@@ -8,18 +8,121 @@ import Contact from './Contact';
 import Vendor from './Vendor';
 import Upcoming from './Upcoming';
 
+const xml = `<?xml version="1.0" encoding="ISO-8859-1"?>
+<feed xmlns="http://www.w3.org/2005/Atom">
+	
+	<title>Latest Documents</title>
+	<link rel="self" href="https://www.patersonnj.gov/egov/api/request.egov?request=feed;dateformat=%25m%2D%25d%2D%25Y;featured=3;title=Latest%20Documents;ctype=4;classificationid=63" />
+	<updated>2021-01-21T09:22:25</updated>
+	<id>https://www.patersonnj.gov/egov/api/request.egov?request=feed;dateformat=%25m%2D%25d%2D%25Y;featured=3;title=Latest%20Documents;ctype=4;classificationid=63</id>
+	<author>
+		<name>Organization Information</name>
+	</author>
+
+		<years>
+		
+			<year>2021</year>
+		
+			<year>2020</year>
+		
+			<year>2019</year>
+  
+			<year>2018</year>
+		
+			<year>2017</year>
+		
+			<year>2016</year>
+		
+			<year>2015</year>
+		
+			<year>2014</year>
+		
+			<year>2013</year>
+		
+			<year>2012</year>
+		
+			<year>2011</year>
+		
+			<year>2010</year>
+		
+			<year>2009</year>
+		
+			<year>2008</year>
+		
+		</years>
+	
+
+	
+		<entry>
+			<title>12-17-2020 BOARD OF ADJUSTMENT REGULAR MEETING CANCELLED DUE TO INCLEMENT WEATHER AND RESCHEDULED TO MARCH 25, 2021 @ 7:30 P.M.</title>
+			<link rel="alternate" href="https://www.patersonnj.gov/egov/apps/document/center.egov?view=item;id=4175" />
+			<updated>2020-12-16T20:11:49</updated>
+			<id>https://www.patersonnj.gov/egov/apps/document/center.egov?view=item;id=4175</id>
+      <featured>1</featured>
+			
+			
+			<classification_name>News &amp; Notices</classification_name>
+			<classification_id>62</classification_id>
+		</entry>
+	
+		<entry>
+			<title>1-28-2021 BOARD OF ADJUSTMENT REGULAR MEETING AGENDA @ 7:30 P.M.</title>
+			<link rel="alternate" href="https://www.patersonnj.gov/egov/apps/document/center.egov?view=item;id=4238" />
+			<updated>2021-01-19T16:59:51</updated>
+			<id>https://www.patersonnj.gov/egov/apps/document/center.egov?view=item;id=4238</id>
+      <featured>1</featured>
+			<classification_name>News &amp; Notices</classification_name>
+			<classification_id>62</classification_id>
+		</entry>
+	
+		<entry>
+			<title>1-26-2021 PLANNING BOARD SPECIAL MEETING AGENDA @ 6:30 P.M.</title>
+			<link rel="alternate" href="https://www.patersonnj.gov/egov/apps/document/center.egov?view=item;id=4236" />
+			<updated>2021-01-19T15:36:06</updated>
+			<id>https://www.patersonnj.gov/egov/apps/document/center.egov?view=item;id=4236</id>
+      <featured>0</featured>
+			<classification_name>News &amp; Notices</classification_name>
+			<classification_id>62</classification_id>
+		</entry>
+	
+		<entry>
+			<title>1-26-2021 PLANNING BOARD SPECIAL MEETING AGENDA @ 7:00 P.M.</title>
+			<link rel="alternate" href="https://www.patersonnj.gov/egov/apps/document/center.egov?view=item;id=4237" />
+			<updated>2021-01-19T15:34:43</updated>
+			<id>https://www.patersonnj.gov/egov/apps/document/center.egov?view=item;id=4237</id>
+      <featured>0</featured>
+			
+			
+			<classification_name>News &amp; Notices</classification_name>
+			<classification_id>62</classification_id>
+		</entry>
+		<entry>
+			<title>HPC 1/25/2021 Regular Meeting Agenda</title>
+			<link rel="alternate" href="https://www.patersonnj.gov/egov/apps/document/center.egov?view=item;id=4243" />
+			<updated>2021-01-21T09:22:25</updated>
+			<id>https://www.patersonnj.gov/egov/apps/document/center.egov?view=item;id=4243</id>
+      <featured>0</featured>
+			<classification_name>Agendas</classification_name>
+			<classification_id>63</classification_id>
+		</entry>
+</feed>
+
+`
 function App() {
   const [selectedTab, setSelectedTab] = useState('open');
   const [data, setData] = useState({});
 
   useEffect(() =>{
-    const proxyurl = "";
+/*     const proxyurl = "https://cors-anywhere.herokuapp.com/";
     const url = "https://www.patersonnj.gov/egov/api/request.egov?request=feed;dateformat=%25m-%25d-%25Y;title=Latest%20Documents;ctype=4;classificationid=63;classificationid=67;count=5"; // site that doesn’t send Access-Control-*
     fetch(proxyurl + url) //
     .then(response => response.text())
     .then(contents => setData(JSON.parse(convert.xml2json(contents, {compact: true, spaces: 4}))))
-    .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))  
-})
+    .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?")) */
+    const jsonData = JSON.parse(convert.xml2json(xml, {compact: true, spaces: 4}))
+    setData(jsonData)
+}, [])
+
   return (
     <div className='center' style={{backgroundColor:'#E5E5E5'}}>
       <div
@@ -73,7 +176,7 @@ function App() {
           </div>
         </header>
       </div>
-      {selectedTab === 'open' && <OpenOpportunities feeds={data?.feeds}/>}
+      {selectedTab === 'open' && <OpenOpportunities feeds={data?.feed?.entry}/>}
       {selectedTab === 'upcoming' && <Upcoming />}
       {selectedTab === 'vendor' && <Vendor />}
       {selectedTab === 'contact' && <Contact />}
--- src/OpenOpportunities.js
+++ src/OpenOpportunities.js
@@ -1,9 +1,32 @@
 import { useState } from 'react';
+import { format, addMonths } from 'date-fns'
 
-const OpenOpportunities = (feeds) => {
+const OpenOpportunities = ({feeds}) => {
 
   const [inputText, setInputText] = useState('')
   console.log(feeds)
+
+  const renderFeeds = () => {
+
+    return feeds.map((feed, i) => {
+      const datePosted = format(new Date(feed.updated._text), 'MM/dd/yy')
+      const responseDeadline = format(addMonths(new Date(feed.updated._text),1), 'MM/dd/yy')
+
+        return (
+          <div key={`${i}${feed.title._text}`} className='bg-white pa3 mt3 br1'>
+            <h3 className='ma2'>{feed.title._text}</h3>
+            <div className='ma2 mt3'>{feed.classification_id._text}</div>
+            <div className='ma2 mt3'>Purchasing</div>
+            <div className='ma2 mt4'>Response Deadline: {responseDeadline}</div>
+            <div className='ma2'>Date Posted: {datePosted}</div>
+            <div className='flex-row ma2 mt4 '>
+                <a className='bg-dark-green white pa2 br2 b' href={feed.link._attributes.href} target='_blank' rel='noreferrer'>View Description</a>
+                <a className='dark-blue pa2 ml2 b' href={feed.link._attributes.href} target='_blank' rel='noreferrer'>Download Submission Files</a>
+            </div>
+          </div>
+        )
+      })
+  }
 
   return (
     <div className='ml5-l mr5-l ml2 mr2' style={{ height: '55%' }}>
@@ -25,15 +48,13 @@ const OpenOpportunities = (feeds) => {
           Browse
         </button>
       </div>
-      <div>
+      <div className='mt4 f4 b'>
         Your Results
       </div>
       <div>
         Showing available opportunities
       </div>
-      <div className='bg-white pa3'>
-        <div></div>
-      </div>
+      {feeds && renderFeeds()}
   </div>
   );
 };
